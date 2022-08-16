dc4d2a2092d4223c4f28a509b6db0bb817174373
IslamnurovRR <rustick007@gmail.com>
2022-08-16 15:44:01 +0300

    Handler was replaced by runOnUIThread method

--- app/src/main/java/com/teaching/coroutines/MainActivity.kt
+++ app/src/main/java/com/teaching/coroutines/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.teaching.coroutines
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.os.ProxyFileDescriptorCallback
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.teaching.coroutines.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
@@ -22,15 +21,12 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.isEnabled = true
         loadCity {
             binding.tvLocation.text = it
-            val temp = loadTemperature(it) {
+             loadTemperature(it) {
                 binding.tvTemperature.text = it.toString()
                 binding.progress.isVisible = false
                 binding.buttonLoad.isEnabled = true
             }
-
         }
-
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,21 +40,18 @@ class MainActivity : AppCompatActivity() {
     private fun loadCity(callback: (String) -> Unit) {
 
         thread {
-            Handler(Looper.getMainLooper()).post {
+            runOnUiThread {
                 Thread.sleep(5000)
 
                 callback.invoke("Moscow")
             }
         }
-
-
     }
 
     private fun loadTemperature(city: String, callback: (Int) -> Unit) {
         thread {
-            Handler(Looper.getMainLooper()).post {
-
-                Toast.makeText(
+           runOnUiThread {
+               Toast.makeText(
                     this,
                     "Loading temperature for city: ${city}",
                     Toast.LENGTH_SHORT
@@ -68,10 +61,6 @@ class MainActivity : AppCompatActivity() {
             Thread.sleep(5000)
 
             Handler(Looper.getMainLooper()).post { callback.invoke(17) }
-
-
         }
-
-
     }
 }
